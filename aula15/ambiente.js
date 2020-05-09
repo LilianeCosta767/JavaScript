197033a7ef0224f15dac035760af65b9b30c24ae
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-09 15:38:21 -0300

    arquivos da aula 15 de javascript com Gustavo Guanabara

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9 ,3]
+num.push(7) // acrescenta um valor no final do vetor
+//num.length
+num.sort() // deixa o vetor em ordem crescente
+//num.indexOf(7) // retorna a chave onde o valor entre () se encontra
+let pos = num.indexOf(4)
+
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O valor 8 está na posição ${pos}`)
