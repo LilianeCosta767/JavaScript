9648334491096e293874646a39f8f4abdfb2362e
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-26 23:40:01 -0300

    interacao com o usuario, execucao de conta simples e otimizacao no print

---
+++ DESAFIOS/desafio03/script.js
@@ -0,0 +1,4 @@
+function pegavalor() {
+    num = parseInt(prompt('Digite um número inteiro qualquer'))
+    window.alert(`Antes de ${num}, temos o número ${num-1}.\nDepois de ${num}, temos o número ${num+1}`)
+}
