d7e939c144473dd0459a2ea11ab8036b9745b374
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-26 23:14:38 -0300

    criacao de sistema simples com botoes interativos

---
+++ DESAFIOS/desafio01/script.js
@@ -0,0 +1,11 @@
+function funcao1() {
+    window.alert('Você clicou no 1° botão!')
+}
+
+function funcao2() {
+    window.alert('Você clicou no 2° botão!')
+}
+
+function funcao3() {
+    window.alert('Você clicou no 3° botão!')
+}
