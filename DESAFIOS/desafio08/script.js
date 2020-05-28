e0a25620152f7fdec6a0b87026b27992bdf750f9
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-28 10:19:46 -0300

    insercao de comentarios

--- DESAFIOS/desafio08/script.js
+++ DESAFIOS/desafio08/script.js
@@ -1,16 +1,16 @@
-let res = document.getElementById('res')
-let paragrafo = document.getElementById('paragrafo')
+let res = document.getElementById('res') //pega a variavel res do arquivo html que é um id
 
 function dados() {
-    let produto = prompt('QUal é o produto que você está comprando?')
-    let preco = parseFloat(prompt(`Qual é o preço de ${produto}?`))
-    calculo(produto, preco)
+    let produto = prompt('QUal é o produto que você está comprando?') //o prompt é uma forma de pegar informações do usuario
+    let preco = parseFloat(prompt(`Qual é o preço de ${produto}?`)) // prompt envolto com parseFloat faz com que o que foi digitado seja convertido para float
+    calculo(produto, preco) //faz chamada para outra função
 }
 
 function calculo( produto,  preco) {
     let desconto = 0.1 * preco
     let valor_final = preco - desconto
 
+    //o res.innerHTML substitui a área do id res lá do arquivo html
     res.innerHTML = ''
     res.innerHTML += `<h1>Calculando desconto de 10% para ${produto}</h1>`
     res.innerHTML += '</br>'
