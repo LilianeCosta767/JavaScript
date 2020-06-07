a2b02d2b73ea2945fb8b89c7c3a08d2d28cd2242
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-06-06 21:35:15 -0300

    base da calculadora quase pronta

--- CALCULADORA/script.js
+++ CALCULADORA/script.js
@@ -1,119 +1,104 @@
 var visor = window.document.getElementById('visor')
+
+var numb1_vec = []
+var numb1
+var cont_numb1 = 0
+
+var numb2_vec = []
+var numb2
+var cont_numb2 = 0
+
+var exp = []
+var resul
 visor.innerHTML = ''
 
-var operation = ["", "", ""]
-number = ""
-var result
+console.log('teste = ', exp[0])
 
-function f_number(num) {
-    number += num
-    // console.log('number = ', number)
-    show_result(number)
-}
-
-function f_operation(op) {
-    if(op != "="){
-        show_result(op)
+function insert_number(num) {
+    // receber o primeiro numero, antes de um operador
+    if(exp[0] == undefined){
+        numb1_vec.push(num)
+        cont_numb1++
+        show_numb1_vec()
     }
-    // começando com um operador
-    if (operation[0] == "" && number == "") {
-        operation[0] = op
-    }
-    // começando com um numero, entao, em seguida vem um operador
-    else if (operation[0] == "" && number != "") {
-        operation[0] = Number(number)
-        operation[1] = op
-        number = ""
-        console.log('op = ', op)
-    }
-    // quando aperta igual
-    else if (op == "=") {
-        // digitação de outro numero
-        if (operation[2] == "" && number != "") {
-            operation[2] = Number(number)
-            number = ""
-            make_account()
-        }
-        // operação com as 3 posições
-        else if (operation[2] != ""){
-            make_account()
-        }
-        // operação com 2 posições
-        else if (operation[2] == "") {
-            // posição 0 ocupada, number nao esta vazio e a posicao 0 não é um número
-            if (number != "" && operation[0] != "") {
-                operation[1] = Number(number)
-                number = ""
-                make_account()
-            }
-            // caso de na posição 0 ser um operador e na posicação 1 ser um numero
-            console.log('operation = ', operation)
-            make_account(op)
-        }
-        show_result()
-        clear_all()
+    // para receber o segundo numero após o operador
+    else if (exp[1] != undefined) {
+        numb2_vec.push(num)
+        cont_numb2++
+        show_numb2_vec()
     }
 }
 
-function make_account() {
-    switch(operation[1]){
-        case '%': 
-            result = operation[0] / 100
-            console.log('result = ', result)
-            break;
-        case '+':
-            result = operation[0] + operation[2]
-            console.log('result = ', result)
-            break;
-        case '-':
-            result = operation[0] - operation[2]
-            console.log('result = ', result)
-            break;
-        case '*':
-            result = operation[0] * operation[2]
-            console.log('result = ', result)
-            break;
-        case '/':
-            result = operation[0] / operation[2]
-            console.log('result = ', result)
-            break;
-        case '^':
-            result = Math.pow(operation[0], operation[2])
-            console.log('result = ', result)
-            break;
-        default:
-            if(operation[0] == '√'){
-                result = Math.sqrt(operation[1])
-                console.log('result = ', result)
-            }
-            else{
-                console.log('nao deu certo')
-            }
+function insert_operator(op) {
+    // expressão começa com numero
+    if(numb1_vec.length != 0 && numb2_vec.length == 0) {
+        console.log('entreii aqui') // TESTE
+        exp.push(numb1)
+        exp.push(op)
+        console.log('exp = ', exp)
+    }
+
+    // verificação do operador =
+    if (exp[1] != undefined && numb2_vec.length != 0 && op == "=") {
+        console.log('antes do numb2, exp = ', exp)
+        exp.push(numb2)
+        console.log('depois do numb2, exp = ', exp)
+        show_exp()
+    }
+
+    if (op == "=") {
+        make_account()
     }
 }
 
-// function is_number(num) {
-//     if(typeof(num) == typeof(0)) {
-//         return true
-//     } else {
-//         return false
-//     }
-// }
-
-function clear_all () {
-    operation = ["", "", ""]
-    number = ""
-    result = ""
+function show_numb1_vec () {
+    var texto = ""
+    for (var i = 0; i < cont_numb1; i++){
+        texto += numb1_vec[i] + ""
+    }
+    numb1 = Number(texto)
+    // console.log('numb1 = ', texto)
 }
 
-function clear_one () {
-    number = number.substring(0,(number.length - 1));
-    console.log('number = ', number)
-    show_result(number)
+function show_numb2_vec () {
+    var texto = ""
+    for (var i = 0; i < cont_numb2; i++){
+        texto += numb2_vec[i] + ""
+    }
+    numb2 = Number(texto)
+    // console.log('numb2 = ', texto)
+    
 }
 
-function show_result(imp) {
-    // visor.innerHTML = ''
-    // visor.innerHTML += `${operation[0]} ${operation[1]} ${operation[2]} `
-    visor.innerHTML += `${imp}`
+function show_exp () {
+    var texto = ""
+    for (var i = 0; i < exp.length; i++){
+        texto += exp[i] + ""
+    }
+    console.log('exp final = ',texto)
+}
+
+function make_account () {
+    switch (exp[1]) {
+        case "+":
+            resul = exp[0] + exp[2]
+            console.log('resultado = ', resul)
+            break;
+        case "-":
+            resul = exp[0] - exp[2]
+            console.log('resultado = ', resul)
+            break;
+        case "/":
+            resul = exp[0] / exp[2]
+            console.log('resultado = ', resul)
+            break;
+        case "*":
+            resul = exp[0] * exp[2]
+            console.log('resultado = ', resul)
+            break;
+        case "^":
+            resul = Math.pow(exp[0], exp[2])
+            console.log('resultado = ', resul)
+            break;
+    }
 }
